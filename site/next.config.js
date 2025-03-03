e12fedc2f4304270c4109cb32f6f7559f4c46610
Nova-Prime <mrbaskin75@gmail.com>
2025-03-02 19:01:44 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -3,14 +3,14 @@
 const nextConfig = {
   env: {
     name: 'AF1 Kasm Registry',
-    description: 'Collection of Workspace Tools for Testing within Home Lab Environment.',
+    description: 'Collection of Workspace Tools for Testing within Home Lab Environment',
     icon: 'https://avatars.githubusercontent.com/u/201309878?s=96&v=4',
-    listUrl: 'https://AppliedForce1.github.io/AF1-kasm-registry',
+    listUrl: 'https://AppliedForce1.github.io/AF1-kasm-registry/',
     contactUrl: 'https://github.com/AppliedForce1/AF1-kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/AF1-kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
